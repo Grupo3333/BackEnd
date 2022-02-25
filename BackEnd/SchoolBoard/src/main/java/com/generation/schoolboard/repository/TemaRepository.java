a82c0392c5f076524e2e0dd089b4a721345ff135
Gifaela <giulia-rafa@hotmail.com>
2022-02-25 11:41:14 -0300

    Criando método CRUD

---
+++ BackEnd/SchoolBoard/src/main/java/com/generation/schoolboard/repository/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.generation.schoolboard.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.schoolboard.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema>findAllByTemaContainingIgnoreCase(String tema);
+	public List<Tema>findAllByNivelContainingIgnoreCase(String nivel);
+	public List<Tema>findAllByTemaContainingIgnoreCaseAndNivelIgnoreCase(String tema, String nivel);
+
+}
