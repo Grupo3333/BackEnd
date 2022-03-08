73602093e1a5b2aa77de11af9bd1917e97f44c08
dennerv <denner.vieira@live.com>
2022-03-08 15:53:52 -0300

    Atualização

--- BackEnd/SchoolBoard/src/main/java/com/generation/schoolboard/model/Tema.java
+++ BackEnd/SchoolBoard/src/main/java/com/generation/schoolboard/model/Tema.java
@@ -28,7 +28,7 @@ public class Tema {
 	private String nivel;
 	
 
-	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("tema")
 	private List <Postagem> postagem;
 	
