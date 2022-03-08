9c1b0b1be9fcc603a01baea8f8a469660506fe4d
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-03-08 11:56:26 -0300

    Inicio da UsuarioController feito de cabeça

---
+++ BackEnd/SchoolBoard/src/main/java/com/generation/schoolboard/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.generation.schoolboard.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.schoolboard.model.Usuario;
+import com.generation.schoolboard.repository.UsuarioRepository;
+import com.generation.schoolboard.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll() {
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+}
